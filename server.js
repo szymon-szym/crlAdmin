f78a41bc7820732252707706b338023a42428ed5
szymon-szym <35077381+szymon-szym@users.noreply.github.com>
2018-05-21 22:40:34 +0200

    add and remove functions via buttons

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ function listAllUsers(nextPageToken) {
 //get ver users
 
 function getVerUser () {
+  verUsers = [];
   admin.database().ref('verifiedUsers').once('value')
+  // admin.database().ref('vertest').once('value')
   .then(function (snap) { 
     for (let user in snap.val()) {
       verUsers.push(user)
@@ -56,43 +58,61 @@ function getVerUser () {
     // console.log(getVerUserProm())
     
 //after resolving promises from Firebase set get/ for server
-
-getVerUser();
-listAllUsers()
-.then(function (result) {
-  usersArr = [];
-  for (user of result) {
-    let userRecord = {};
-    userRecord.name = user.email;
-    userRecord.uid = user.uid;
-    usersArr.push(userRecord);
-  }
-  // console.log(usersArr);
-  return usersArr;
-})
-.then(function (result) {
-  let usersArr = result;
-  // console.log(usersArr);
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-    },
-    app.get("/getUsers", function (req, res) {
-      let usersObj = {
-        usersArr,
-        verUsers
+serveUsers = function() {
+    getVerUser();
+    listAllUsers()
+    .then(function (result) {
+      usersArr = [];
+      for (user of result) {
+        let userRecord = {};
+        userRecord.name = user.email;
+        userRecord.uid = user.uid;
+        usersArr.push(userRecord);
       }
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(usersObj));
-
-      // res.send('test')
+      // console.log(usersArr);
+      return usersArr;
     })
-  );
-});
+    .then(function (result) {
+      let usersArr = result;
+      // console.log(usersArr);
+      app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname + '/index.html'));
+        },
+        app.get("/getUsers", function (req, res) {
+          let usersObj = {
+            usersArr,
+            verUsers
+          }
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify(usersObj));
+    
+          // res.send('test')
+        })
+      );
+    });
+  }
+serveUsers();
 
 app.post("/addVer", function (req, res){
   console.log('post');
-  console.log(req.body);
+  // console.log(req.body);
+  let user = req.body;
+  userRef = admin.database().ref('verifiedUsers/' + user.uid);
+  // console.log(userRef);
+  userRef.set({
+    location: user.location
+  })
+  serveUsers();
+})
 
+app.post("/remVer", function (req, res){
+  console.log('rem');
+  // console.log(req.body);
+  let user = req.body;
+  userRef = admin.database().ref('verifiedUsers/' + user.uid);
+  // console.log(userRef);
+  userRef.remove()
+  serveUsers();
 })
 
 app.listen (3000, function() {
